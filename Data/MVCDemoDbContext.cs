5ff0176a5ebacb09a0418d3db1981458f692777f
SwapnilJadhav3011 <swapnil.jadhav3011@gmail.com>
2023-10-06 14:31:08 +0530

    First Commit

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmpApplication.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpApplication.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
